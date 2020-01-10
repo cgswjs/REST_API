e7e00a4eb9e523b79ae99eb89178dcb486904c66
Ian Chen <cgswjs@Ians-MacBook-Pro.local>
2020-01-09 20:32:42 -0800

    Complete RESTful API commands using POSTMAN to test

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const _ = require("lodash");
 
 const app = express()
 
@@ -50,8 +51,10 @@ app.route("/articles")
         }
     });
   })
+
 .delete(
     function(req,res){
+      //delete the entire database if no specific condition provided in {}
       Article.deleteMany(
         {},function(err){
           if(!err){
@@ -62,6 +65,65 @@ app.route("/articles")
         });
     });
 
+//Request targetting specific article//
+app.route("/articles/:customArticleName")
+.get(
+  function(req,res){
+    const articleName = req.params.customArticleName;
+    Article.findOne({title:articleName},function(err,foundArticle){
+      if(foundArticle){
+        res.send(foundArticle)
+      }else{
+        res.send("No article found")
+      }
+    });
+  })
+
+//put request will delete the original object and insert the new object
+.put(
+  function(req,res){
+    const articleName = req.params.customArticleName;
+    Article.update(
+      {title:articleName},
+      {title:req.body.title,content:req.body.content},
+      {overwrite:true},
+      function(err){
+        if(!err){
+          res.send("succefully updated");
+        }else{
+          res.send(err);
+        }
+      }
+    )
+  })
+
+//patch request allows user to partially update object in the database
+.patch(
+  function(req,res){
+    const articleName = req.params.customArticleName;
+    Article.update(
+      {title:articleName},
+      {$set:{title:req.body.title}},
+      function(err){
+        if(!err){
+          res.send("succefully patched database");
+        }else{
+          res.send(err);
+        }
+      })
+  })
+.delete(
+  function(req,res){
+    Article.deleteOne(
+      {title:req.params.customArticleName},function(err){
+        if(!err){
+          res.send("successfully deleted");
+        }else{
+          res.send(err);
+        }
+      }
+    );
+  });
 
 app.listen(3000,function(){
   console.log("Server started on port 3000");
